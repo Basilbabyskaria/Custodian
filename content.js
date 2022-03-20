849a305e91dbf7e6c8bb1475b2c38dc08691aff7
basil baby skaria <basilbabyskaria@gmail.com>
2022-03-20 12:13:33 +0530

    init

--- content.js
+++ content.js
@@ -1,6 +1,5 @@
 answer = confirm ("Bullying Detected! Do you want to report it?");
 if(answer==true)
 {
-    window.open("mail.html", "extension_popup", "width=300,height=400,status=no,scrollbars=no,resizable=no");
-
+    //window.open("mail.html", "extension_popup", "width=300,height=400,status=no,scrollbars=no,resizable=no");
 }
