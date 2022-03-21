cab9c0b74bd0bf1add73d46e856d924f6ec99925
basil baby skaria <basilbabyskaria@gmail.com>
2022-03-21 13:07:31 +0530

    INIT

---
+++ add.php
@@ -0,0 +1,11 @@
+<?php
+$dbServername = "localhost";
+$dbUsername ="root";
+$dbPassword ="";
+$dbName ="bullying_word_database";
+$conn =mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName)
+
+
+
+
+?>
